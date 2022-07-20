2ee6f64f9845cafdcd4e68eafd148da79b689a6a
Ben Cooper <ben.s.cooper@gmail.com>
2022-07-20 16:25:23 +0100

    Add files via upload

--- runall.R
+++ runall.R
@@ -8,26 +8,26 @@ if(runwithsyntheticdata) {
   file3<- "vacc_cov_by_region.csv"
   file4<- "synthetic_vacc_cov_by_region.csv"
   
-  if (file.exists(file1)) {
+  if (file.exists(file1) & !file.exists("realdata_sitreps_eng_expanded.rds")) {
     file.rename(file1, "realdata_sitreps_eng_expanded.rds")
   } else {
     #  do nothing
   }
   
   if (file.exists(file2)) {
-    file.rename(file2, "sitreps_eng_expanded.rds")
+    file.copy(file2, "sitreps_eng_expanded.rds")
   } else {
     cat("Synthetic data file synthetic_sitreps_eng_expanded.rds not found")
   }
   
-  if (file.exists(file3)) {
+  if (file.exists(file3) & !file.exists("realdata_vacc_cov_by_region.csv")) {
     file.rename(file3, "realdata_vacc_cov_by_region.csv")
   } else {
     #  do nothing
   }
   
   if (file.exists(file4)) {
-    file.rename(file4, "vacc_cov_by_region.csv")
+    file.copy(file4, "vacc_cov_by_region.csv")
   } else {
     cat("Synthetic data file synthetic_vacc_cov_by_region.csv not found")
   }
@@ -36,7 +36,7 @@ if(runwithsyntheticdata) {
 
 # Commands to run all analysis here 
 source("preparedata.R")
-ssource("fig1.r")
+source("fig1.r")
 source("fig2.r")
 source("run regression models.r")
 source("fig3.r")
@@ -48,9 +48,9 @@ source("fig S5.r")
 
 # Cleanup afterwards by renaming files back
 if(runwithsyntheticdata) {
-  file.rename("vacc_cov_by_region.csv","synthetic_vacc_cov_by_region.csv" )
-  file.rename("sitreps_eng_expanded.rds","synthetic_sitreps_eng_expanded.rds")
-  file.rename("realdata_vacc_cov_by_region.csv", "vacc_cov_by_region.csv" )
-  file.rename("realdata_sitreps_eng_expanded.rds","sitreps_eng_expanded.rds")
+  # file.rename("vacc_cov_by_region.csv","synthetic_vacc_cov_by_region.csv" )
+  # file.rename("sitreps_eng_expanded.rds","synthetic_sitreps_eng_expanded.rds")
+  file.copy("realdata_vacc_cov_by_region.csv", "vacc_cov_by_region.csv" )
+  file.copy("realdata_sitreps_eng_expanded.rds","sitreps_eng_expanded.rds")
 }
 
