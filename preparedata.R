1d197dadb6ca2f104e2709d81a750cee5566e8b3
Ben Cooper <ben.s.cooper@gmail.com>
2023-02-01 14:11:15 +0000

    Add files via upload

--- preparedata.R
+++ preparedata.R
@@ -1,6 +1,9 @@
 # Prepare data
-
-source("create_sitrep_weekly.R")
+if(runwithsyntheticdata){
+  source("create_sitrep_weekly_synthetic.R")
+} else {
+  source("create_sitrep_weekly.R")
+}
 source("merge_nvdata.R")  #add in new variant data to sitrep_weekly
 source("merge_vaccine_data.R") # add in vaccine coverage (by region from SIREN study)
 source("mergeERICdata.r") #  merge in additional data on trusts such as number of side rooms 
